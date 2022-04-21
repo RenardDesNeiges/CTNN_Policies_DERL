9067d3f1dbc7cb65debc4b4163142698f7e13de4
Titouan Renard <titouan.renard@gmail.com>
2022-04-21 18:15:57 +0200

    LTC implementation (untested)

--- models.py
+++ models.py
@@ -10,9 +10,9 @@ class LeakyComponent(tf.keras.layers.Layer):
     super(LeakyComponent, self).__init__()
 
   def build(self, input_shape):
-    w_init = tf.random_normal_initializer()
+    tau_init = tf.random_normal_initializer(mean=0.0, stddev=0.05)
     self.tau = tf.Variable(
-        initial_value=1-w_init(shape=(1, input_shape[-1]),
+        initial_value=1-tau_init(shape=(1, input_shape[-1]),
                             dtype='float32'),
         trainable=True)
     self.mult = tf.keras.layers.Multiply()
@@ -20,6 +20,26 @@ class LeakyComponent(tf.keras.layers.Layer):
 
   def call(self, x):
     result = self.mult([x,-1/self.tau])
+    return result
+
+  def compute_output_shape(self, input_shape):
+    return input_shape[0]
+
+class ConductanceConnectivity(tf.keras.layers.Layer):
+  def __init__(self):
+    super(ConductanceConnectivity, self).__init__()
+
+  def build(self, input_shape):
+    bias_init = tf.random_normal_initializer(mean=1.0, stddev=0.5)
+    self.bias = tf.Variable(
+        initial_value=bias_init(shape=(1, input_shape[-1]),
+                            dtype='float32'),
+        trainable=True)
+    self.add = tf.keras.layers.Add()
+    super(ConductanceConnectivity, self).build(input_shape)  # Be sure to call this at the end
+
+  def call(self, x):
+    result = self.add([-x,self.bias])
     return result
 
   def compute_output_shape(self, input_shape):
@@ -95,7 +115,6 @@ class ODEMLP(ODEModel):
 
 class CTRNN(ODEModel):
   """ Basic CT-RNN model using the ode wrapper. """
-  # pylint: disable=too-many-arguments
   def __init__(self, output_units, hidden_units=64,
                num_state_layers=1, num_dynamics_layers=1, num_output_layers=1,
                time=(0., 1.), rtol=1e-3, atol=1e-3, tau = 0.9):
@@ -144,7 +163,6 @@ class LTC(ODEModel):
                num_state_layers=1, num_dynamics_layers=1, num_output_layers=1,
                time=(0., 1.), rtol=1e-3, atol=1e-3, tau = 0.9):
 
-    raise Exception("LTC not implemented yed")
 
     def make_sequential(num_layers, **layer_kws):
       return tf.keras.Sequential(
@@ -163,20 +181,23 @@ class LTC(ODEModel):
                      kernel_initializer=tf.initializers.orthogonal(1))
     output = make_sequential(num_output_layers, **layer_kws)
     
-    self.tau = tau
-    
     super().__init__(state, dynamics, output, time=time, rtol=rtol, atol=atol)
+    
+    self.add = tf.keras.layers.Add()
+    self.mult = tf.keras.layers.Multiply()
+    self.leakyComponent = LeakyComponent()
+    self.conductance = ConductanceConnectivity()
     
   # implements a leaky integrator
   def call(self, inputs, training=True, mask=None):
-    raise Exception("LTC not implemented yed")
   
     _ = training, mask
 
     def dynamics(inputs, time):
       time = tf.cast([[time]], tf.float32)
-      inputs_padded = tf.concat([inputs, tf.tile(time, [inputs.shape[0], 1])], -1)
-      return tf.keras.layers.Add()([self.dynamics(inputs_padded),(-1/self.tau)*inputs]) 
+      non_linearity = self.dynamics(tf.concat([inputs, tf.tile(time, [inputs.shape[0], 1])], -1))
+      non_linearity = self.mult([non_linearity,self.conductance(inputs)])
+      return self.add([non_linearity,self.leakyComponent(inputs)])
 
     state = self.state(inputs)
     hidden = self.odeint(dynamics, state, self.time)[-1]
